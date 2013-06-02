b21175a8da4d4ec652da74d49ade2486f69bdb65
arthulia <kirby01@gmail.com>
2013-06-02 16:49:25 -0700

    Added HTML5 localStorage support.

--- js/fridgelist.js
+++ js/fridgelist.js
@@ -1,12 +1,68 @@
+// Save everything.
+
+// Save data to HTML5 Local Storage
+function saveLocal() {
+	var cells = new Object();
+
+	$('#grocery-list tr').each(function(i,data) {
+		var checked = !$(data).children('.checkbox-cell').children('a').children('i').hasClass('check-empty');
+		var listitem = $.trim($(data).children('.grocery-cell').children('.grocery-name').text());
+
+		cells[listitem] = checked;
+
+	});
+	
+	saveString = JSON.stringify(cells);
+
+	localStorage.setItem('cells', saveString);
+
+	//console.log(saveString);
+}
+
+// Restore HTML5 Local Storage
+function restoreLocal() {
+
+	cells = JSON.parse(localStorage.getItem('cells'));
+
+	for (var item in cells) {
+		if (cells.hasOwnProperty(item)) {
+			addItem(cells[item], item);
+			//console.log(item + " -> " + cells[item]);
+		}
+	}
+
+}
+
+// Escape HTML strings
+function escapeHTML(html) {
+	return html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+}
+
+function addItem(checkedBool, listitem) {
+
+		var checked = "icon-check";
+		var gcchecked = "gc-checked";
+
+		if (checkedBool == false) {
+			checked = "check-empty icon-check-empty";
+			gcchecked = "";
+		}
+
+		$('#grocery-list').append('<tr class="grocery-row"><td class="checkbox-cell"><a href="#" class="btn cb-button"><i class="icon-large check ' + checked + '"></i></a></td><td class="grocery-cell ' + gcchecked + '"><div class="grocery-name">' + listitem + '</div></td></tr>');
+
+
+}
+
 $(document).ready(function(){
 
-	var editstate = false;
+	restoreLocal();
 
 	// Normal vs Edit mode.
+	var editstate = false;
   $('.list-button').on('click',function(){
 
 		if (!editstate) {
-			// Normal Mode
+			// Edit Mode
 			$(this).removeClass('btn-info');
 			$(this).addClass('btn-warning');
 			$(this).children('i').removeClass('icon-th-list');
@@ -15,8 +71,10 @@ $(document).ready(function(){
 			$('.check-empty').removeClass('icon-check-empty');
 			$('.check').addClass('icon-trash');
 			$('.check-empty').addClass('icon-trash');
+			$('#add-item-input').attr("disabled", true);
+			$('#add-item-input').val('Return to list mode...');
 		} else {
-			// Edit Mode
+			// Normal Mode
 			$(this).removeClass('btn-warning');
 			$(this).addClass('btn-info');
 			$(this).children('i').removeClass('icon-chevron-left');
@@ -25,23 +83,27 @@ $(document).ready(function(){
 			$('.check-empty').removeClass('icon-trash');
 			$('.check').addClass('icon-check');
 			$('.check-empty').addClass('icon-check-empty');
+			$('#add-item-input').attr("disabled", false);
+			$('#add-item-input').val('');
 		}
 
 		editstate = !editstate;
 
   });
 
-	// Add item
+	// Submit item
 	$('#add-item-form').submit(function() {
-		$('#grocery-list').append('<tr class="grocery-row"><td class="checkbox-cell"><a href="#" class="btn cb-button"><i class="icon-large check check-empty icon-check-empty"></i></a></td><td class="grocery-cell"><div class="grocery-name">' + escapeHTML($(this).children('#add-item-input').val()) + '</div></td></tr>');				
+
+		addItem(false, escapeHTML($(this).children('#add-item-input').val()));
 
 		$(this).children('#add-item-input').val('');
-		return false;
+		
+		saveLocal(); // Save to HTML5 local storage.
+
+		return false; // Disable refresh
+
 	});
 
-	function escapeHTML(html) {
-		return html.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-	}
 });
 
 // Check, uncheck, delete
@@ -66,4 +128,6 @@ $(document).on('click', '.grocery-row', function() {
 		}
 
 	}
+	saveLocal(); // Save to HTML5 local storage.
+
 });
